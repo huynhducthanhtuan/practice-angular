a16e9501a349973b4ecdef69144868ac243465ef
huynhdthanhtuan <tuan40655@gmail.com>
2023-02-10 17:20:53 +0700

    Generate test directive|pipe|service|class|guard|interface|enum|module by ng command.

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-# PracticeAngular
+# Practice AngularJS
 
 [Angular CLI](https://github.com/angular/angular-cli) version 15.1.1.
+
+## Install npm packages
+
+`npm install`
 
 ## Development server
 
 `ng serve`
 <br />
 `http://localhost:4200/`
-
-## Code scaffolding
-
-`ng generate component component-name`
-<br />
-`ng generate directive|pipe|service|class|guard|interface|enum|module`
 
 ## Build
 
@@ -25,6 +23,12 @@
 ## Running end-to-end tests
 
 `ng e2e`
+
+## Code scaffolding
+
+`ng generate component component-name`
+<br />
+`ng generate directive|pipe|service|class|guard|interface|enum|module`
 
 ## Further help
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { TestPipePipe } from './test/test-pipe.pipe';
+import { TestDirectiveDirective } from './test/test-directive.directive';
+import { TestComponentComponent } from './test/test-component/test-component.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeaderComponent,
 		FooterComponent,
-		ContentComponent
+		ContentComponent,
+		TestPipePipe,
+		TestDirectiveDirective,
+		TestComponentComponent
 	],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
